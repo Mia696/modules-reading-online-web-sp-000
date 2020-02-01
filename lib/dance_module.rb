98ac5e6a7bdd94067db0bbf18dbbf84703979394
Mia <tamiapaschall@gmail.com>
2020-02-01 18:16:20 +0000

    Automatically backed up by Learn

--- lib/dance_module.rb
+++ lib/dance_module.rb
@@ -8,10 +8,10 @@ module Dance
   end
   
   def pirouette
-    "I'm doing piourette!"
+    "I'm doing piourette"
   end
   
   def take_a_bow
-    "Thank you, thank you. It was a pleasure to dance for you all!"
+    "Thank you, thank you. It was a pleasure to dance for you all."
   end
 end
